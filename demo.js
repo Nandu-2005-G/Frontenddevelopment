40b6b3baab1c532c275ec6fbcb205d64c5672774
Nandu-2005-G <nandhugnandhini@gmail.com>
2025-07-05 17:24:44 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,11 @@
+ var box1=document.getElementById("num1")
+    var box2=document.getElementById("num2")
+    var result=document.getElementById("result")
+     var box1value=Number(box1.value)
+     var box2value=Number(box2.value)
+     var total=box1value+box2value
+     function resultfunction(){
+        result.textContent=total;
+     }
+     var a=document.querySelector("#hi")
+a.textContent="HI EVERYONE"
